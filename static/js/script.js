8d602bafc6e225b62e88c0a19c90de4a338d16ad
Mark Petersen <fuide92@gmail.com>
2018-04-18 15:43:29 +0200

    added some jquery for active links and scroll

---
+++ static/js/script.js
@@ -0,0 +1,15 @@
+// FIXED MENU AFTER SCROLL
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 700) {
+       $('nav').addClass('nav-bg');
+    }
+    else {
+       $('nav').removeClass('nav-bg');
+    }
+});
+
+// ACTIVE NAV-LINK
+$(document).ready(function() {
+    $('li.active').removeClass('active');
+    $('a[href="' + location.pathname + '"]').closest('li').addClass('active'); 
+});
