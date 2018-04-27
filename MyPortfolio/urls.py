bc4d72d15ec0d6574bf56159309707671fce3686
Mark Petersen <fuide92@gmail.com>
2018-04-27 15:47:42 +0200

    added robots.txt and sitemap.xml

--- MyPortfolio/urls.py
+++ MyPortfolio/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 from . import views
 
 urlpatterns = [
@@ -26,4 +27,6 @@ urlpatterns = [
     path('projekter/', include('project.urls')),
     path('kontakt/', include('contact.urls')),
     path('om/', include('about.urls')),
+    path('robots.txt/', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('sitemap.xml/', TemplateView.as_view(template_name='sitemap.xml', content_type='xml')),
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
