735d2233d2a5eb1471b0c0ae36c105b06e7938eb
Mark Petersen <fuide92@gmail.com>
2018-04-27 19:10:27 +0200

    medium editor and gitignore changes

--- project/admin.py
+++ project/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
+from mediumeditor.admin import MediumEditorAdmin
 from project.models import Project
 
-
-class ProjectAdmin(admin.ModelAdmin):
+class ProjectAdmin(MediumEditorAdmin, admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    mediumeditor_fields = ('description')
 
 admin.site.register(Project, ProjectAdmin)
