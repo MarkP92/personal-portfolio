7408656332dd860f06f5851de8f1cb47934e8807
Mark Petersen <fuide92@gmail.com>
2018-04-27 16:46:27 +0200

    force https

--- MyPortfolio/settings.py
+++ MyPortfolio/settings.py
@@ -158,3 +158,9 @@ DATABASES['default'].update(db_from_env)
 # https://warehouse.python.org/project/whitenoise/
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# SSL and security
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
