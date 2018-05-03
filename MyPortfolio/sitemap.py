36aa528b2ad83436becc4248cc97bb8bf0931147
Mark Petersen <fuide92@gmail.com>
2018-05-03 14:26:27 +0200

    added dynamic sitemap

---
+++ MyPortfolio/sitemap.py
@@ -0,0 +1,44 @@
+from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
+from blog.models import Post
+from project.models import Project
+ 
+class StaticSiteMap(Sitemap):
+ 
+    def items(self):
+        return [
+            'home',
+            'about',
+            'contact',
+            'send',
+        ]
+ 
+    def location(self, item):
+        return reverse(item)
+
+class PostSiteMap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.8
+
+    def items(self):
+        return Post.objects.all()
+
+    def lastmod(self, obj):
+        return obj.published_at
+
+    def location(self, item):
+        return reverse('post_detail', args=[item.slug])
+
+class ProjectSiteMap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.7
+
+    def items(self):
+        return Project.objects.all()
+
+    def lastmod(self, obj):
+        return obj.created_at
+
+    def location(self, item):
+        return reverse('project_detail', args=[item.slug])
+    
--- MyPortfolio/urls.py
+++ MyPortfolio/urls.py
@@ -18,7 +18,15 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.contrib.sitemaps.views import sitemap
 from . import views
+from .sitemap import PostSiteMap, StaticSiteMap, ProjectSiteMap
+
+sitemaps = {
+    'post' : PostSiteMap,
+    'static': StaticSiteMap,
+    'project': ProjectSiteMap,
+}
 
 urlpatterns = [
     path('', views.HomePage.as_view(), name="home"),
@@ -27,6 +35,6 @@ urlpatterns = [
     path('projekter/', include('project.urls')),
     path('kontakt/', include('contact.urls')),
     path('om/', include('about.urls')),
-    path('robots.txt/', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
-    path('sitemap.xml/', TemplateView.as_view(template_name='sitemap.xml', content_type='xml')),
+    path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type='text/plain')),
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap'),
 ]   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
