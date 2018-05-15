d283dcd4c00add3908e8c877ac9ae74bc1f01890
Mark Petersen <fuide92@gmail.com>
2018-05-15 13:09:46 +0200

    changes to js, css and added images

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,3 @@
-
 // ACTIVE NAV-LINK
 $(document).ready(function() {
     $('li.active').removeClass('active');
@@ -6,17 +5,37 @@ $(document).ready(function() {
 });
 
 // FADE IN ANIMATION FOR ABOUT SECTION
-$(window).scroll(function () {
-    console.log($(window).scrollTop());
-    var topDivHeight = $(".hero-section").height();
-    var viewPortSize = $(window).height();
+// $(window).scroll(function () {
+//     var topDivHeight = $(".hero-section").height();
+//     var viewPortSize = $(window).height();
     
-    var triggerAt = 400;
-    var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
+//     var triggerAt = 400;
+//     var triggerHeight = (topDivHeight - viewPortSize) + triggerAt;
 
-    if ($(window).scrollTop() >= triggerHeight) {
-        $('#about-text').css('visibility', 'visible').hide().fadeIn();
-        $('.skills').css('visibility', 'visible').hide().fadeIn(2500);
-        $(this).off('scroll');
-    }
+//     if ($(window).scrollTop() >= triggerHeight) {
+//         $('#about-text').css('visibility', 'visible').hide().fadeIn();
+//         $('.skills').css('visibility', 'visible').hide().fadeIn(2500);
+//         $(this).off('scroll');
+//     }
+// });
+
+// FIXED HEADER ON SCROLL UP
+$(document).ready(function() {
+    var previousScroll = 0,
+        headerOffset = $('#menu').offset().top;
+    $('#menu-wrap').height($('#menu').height());
+    $(window).scroll(function() {
+        var currentScroll = $(this).scrollTop();
+        if(currentScroll > headerOffset) {
+            if (currentScroll > previousScroll) {
+                $('#menu').fadeOut();
+            } else {
+                $('#menu').fadeIn();
+                $('#menu').addClass('fixed');
+            }
+        } else {
+             $('#menu').removeClass('fixed');   
+        }
+        previousScroll = currentScroll;
+    });
 });
